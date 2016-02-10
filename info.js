ad632f4882d41d796dc5788111aadeac736333ee
blindthief10 <kostasdiakogiannis23@gmail.com>
2016-02-10 18:02:04 +0200

    three o'clock

--- info.js
+++ info.js
@@ -3,3 +3,15 @@ var something = document.body.children;
 (function tester() {
 		console.log(something[0].children[0].children[2].nodeType);
 })();
+
+function rightNextToIt() {
+	var count = 0;
+	function rendering() {
+		count++;
+		return count;
+	}
+	return rendering;
+} 
+
+var result = rightNextToIt();
+console.log(result());
