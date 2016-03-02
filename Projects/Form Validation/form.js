5e257083966aaab0dece671b177972932a33d779
blindthief10 <kostasdiakogiannis23@gmail.com>
2016-03-02 14:28:30 +0200

    last of the day

--- Projects/Form Validation/form.js
+++ Projects/Form Validation/form.js
@@ -9,8 +9,33 @@ function checkValidity(pattern,selector) {
 			if (selector.val().match(pattern)) {
 				return 'Your email was valid! Well done!';
 			}else {
-				return 'Invalid email!';
+				return 'Ποιον πας να κοροιδέψεις ρε καραγκιόζη; Σοβαρέψου!!';
 			}
 		}
 
 var emailPattern = /^[\w\.]{2,20}@(gmail|yahoo|hotmail)\.(com|gr|net)$/ig;
+
+var app = {
+	getWeather: function(location) {
+		var promise = $.Deferred();
+		$.ajax('/weather',{
+			type:'GET',
+			data: {obj:location},
+			dataType:'json',
+			success: function(response) {
+				promise.resolve(response.weather);
+			},
+			error:function(data) {
+				promise.reject(data);
+			}
+		});
+		return promise;
+	}
+}
+
+$('selector').on('click',function() {
+	var loc = $('select').data('location');
+	app.getWeather(loc).done(function() {
+		$('sel').html(response);
+	});
+})
