a7f0ecc2631d112870a92b2841f5b8bccd193f85
unknown <kostasdiakogiannis23@gmail.com>
2016-03-04 21:18:31 +0200

    edit

--- Udemy/CodeEditor/code.js
+++ Udemy/CodeEditor/code.js
@@ -1,3 +1,56 @@
 $(document).ready(function() {
-	
-})
+		calculateHeight('.codeContainer');
+		$('#css').on('click',cssShow);
+		$('#html').on('click',htmlShow);
+		$('#javascript').on('click',javascriptShow);
+		$('#result').on('click',resultShow);
+		$('#runButton').on('click',runShow);
+});
+
+function roar() {
+	return $('.active').length;
+}
+
+function calculateHeight(selector) {
+	 var windowHeight = $(window).height(),
+		menuHeight = $('#menu').height(),
+		codeContainerHeight = windowHeight - menuHeight;
+		var some = $(selector).height(codeContainerHeight);
+		return some;
+}
+
+function cssShow() {
+		$(this).toggleClass('active');
+		$('#cssContainer').toggle();
+		var count = roar();
+		$('.codeContainer').css('width',100/count + '%');
+}
+
+function htmlShow() {
+		$(this).toggleClass('active');
+		$('#htmlContainer').toggle();
+		var count = roar();
+		$('.codeContainer').css('width',100/count + '%');
+}
+
+function javascriptShow() {
+		$(this).toggleClass('active');
+		$('#javascriptContainer').toggle();
+		var count = roar();
+		$('.codeContainer').css('width',100/count + '%');
+}
+
+function resultShow() {
+		$(this).toggleClass('active');
+		$('#resultContainer').toggle();
+		var count = roar();
+		$('.codeContainer').css('width',100/count + '%');
+}
+
+function runShow() {
+	$('iframe').contents().find('html').html('<style>'+$('#cssCode').val() + '</style>'+($('#htmlCode').val()));
+	document.getElementById('inlineFrame').contentWindow.eval($('#javascriptCode').val());
+}
+
+
+
