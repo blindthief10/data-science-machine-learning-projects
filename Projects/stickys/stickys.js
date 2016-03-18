dddb9d7d57194f640e498a12b45be34210a663da
unknown <kostasdiakogiannis23@gmail.com>
2016-03-19 00:23:08 +0200

    adding sticky

--- Projects/stickys/stickys.js
+++ Projects/stickys/stickys.js
@@ -1,14 +1,39 @@
 $(document).ready(function() {
 	var formContent = '<div class="modal">' +
-			'<div class="closebutton"></div>' +
+			'<span class="closebutton"></span>' +
 			'<label id="texter" for="title">Title\'s Note</label><br>' +
 			'<input type="text" name="title" placeholder="Put your title here">' +
 			'<label id="area" for="text">Note\'s Body Text</label><br>' +
 			'<textarea name="text"></textarea>' +
-			'<button class="btn">Save Note and Exit</button></div>';
+			'<button class="btn">Save Note and Exit</button></div>', speed = 500;
 	
 	$('#newnote').on('click',function() {
 		$('body').append('<div class="overlay"></div>');
-		$('body').append(formContent).find('.modal').fadeIn(350);
+		$('body').append(formContent).find('.modal').fadeIn(speed);
+		$('.closebutton').on('click',function() {
+			$(this).closest('body').find('.overlay').fadeOut(speed);
+			$(this).parent().fadeOut(speed);
+			setTimeout(function() {
+				$('.overlay').remove();
+				$('.modal').remove();
+			},1000);
+		});
+		$('.btn').on('click',function() {
+			var title = $('input').val(), text = $('textarea').val();
+			if(title === '' || text === '') {
+				alert('You must include a title and a text for your note');
+			}else {
+				$(this).closest('body').find('.overlay').fadeOut(speed);
+				$(this).parent().fadeOut(speed);
+				setTimeout(function() {
+					$('.overlay').remove();
+					$('.modal').remove();
+			},1000);
+				$(this).closest('body').append('<div class="note"></div>');
+			}
+		});
 	});
 });	
+
+
+
---
+++ Projects/stickys/test.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$('.closebutton').on('click',function() {
+		$('.modal').hide();
+	});
+});
