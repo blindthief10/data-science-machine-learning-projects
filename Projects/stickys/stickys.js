49bb3ef24ca1a911730f36778cb4895f401bbc4c
unknown <kostasdiakogiannis23@gmail.com>
2016-03-24 07:23:27 +0200

    stickys without comments

--- Projects/stickys/stickys.js
+++ Projects/stickys/stickys.js
@@ -2,25 +2,17 @@ $(document).ready(function() {
 	if (localStorage.getItem('notes')) {
 		count = JSON.parse(localStorage.getItem('count'));
 		obj = JSON.parse(localStorage.getItem('notes'));
-		console.log(obj);
-		console.log(count);
 	for (var key in JSON.parse(localStorage.getItem('notes'))) {
 	$('body').append('<div class="note" data-role="'+JSON.parse(localStorage.getItem('notes'))[key].datarole+'">'+
 		'<div onclick="another(this.parentNode);" class="clojure"></div><h4>'+JSON.parse(localStorage.getItem('notes'))[key].title+'</h4><p>'+JSON.parse(localStorage.getItem('notes'))[key].text+'</p>'+
 		'<button class="edit" onclick="refer(this.parentNode); editNote(fadeSpeed);">Edit Note</button></div>');
 	}
 }
-	// When clicking the Add Sticky Button create the Modal Form Window 
-	// and all the functionality by executing the showForm function!
 
 	$('#newnote').on('click',function() {
 		showForm(formContent,fadeSpeed);
 	});
-
-	
 });
-// This is the content that is going to be created each time you try to create a new sticky. 
-// It creates the modal window and the form inside with empty text
 
 var formContent = '<div class="modal">' +
 			'<span class="closebutton"></span>' +
@@ -42,20 +34,11 @@ function another(referred) {
 	}
 	$(referred).hide().remove();
 	localStorage.setItem('notes',JSON.stringify(obj));
-	console.log(JSON.parse(localStorage.getItem('notes')));
 }
-/*The refer function help us keeping a reference with specific element by adding a class to it so it can be easily accessible later.
-We do this because we want to have access to the element with inline Javascript. 
-*/
 
 function refer(reference) {
 		$(reference).addClass('something');
-				}
-
-/*This the modal window but with a little bit different content than this of the showForm function.
-This is because the content that is loaded is the same but the input and textarea fields are filled with
-the current value. Note that this function is triggered only when the users wants to update the note!
-*/
+	}
 
 function editNote(speed) {
 	var updateContent = '<div class="modal">' +
@@ -96,7 +79,6 @@ function editNote(speed) {
 				}
 			}
 			localStorage.setItem('notes',JSON.stringify(obj));
-			console.log(JSON.parse(localStorage.getItem('notes')));
 			$('.note').find('.edit').next().remove();
 			$('.something').removeClass('something');
 			$('.close').on('click',function() {
@@ -108,17 +90,10 @@ function editNote(speed) {
 				}
 				$('.localtempo').hide().remove();
 				localStorage.setItem('notes',JSON.stringify(obj));
-				console.log(JSON.parse(localStorage.getItem('notes')));
 			});	
 		}
 	});
 }
-
-/*This is the function that creates the modal window and the form in it's empty mode. The function is triggered
-when the users wants to create a new note and Add a new sticky! This function creates also all the buttons and
-actions that are created from it and are nested to it.
-*/
-
 
 function showForm(content,speed) {	
 	$('body').append('<div class="overlay"></div>');
@@ -147,7 +122,6 @@ function showForm(content,speed) {
 			obj['var'+count] = {datarole:count, title:title, text:text};
 			localStorage.setItem('notes',JSON.stringify(obj));
 			localStorage.setItem('count',JSON.stringify(count));
-			console.log(JSON.parse(localStorage.getItem('notes')));
 			$('.note').find('.edit').next().remove();
 			$('.close').on('click',function() {
 				$(this).parent().addClass('temporary')
@@ -158,7 +132,6 @@ function showForm(content,speed) {
 				}
 				$('.temporary').hide().remove();
 				localStorage.setItem('notes',JSON.stringify(obj));
-				console.log(JSON.parse(localStorage.getItem('notes')));
 			});	
 		}
 	});
