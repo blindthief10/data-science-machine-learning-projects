9b907adbca81399d9ad96c8c9e5fe097033bf890
unknown <kostasdiakogiannis23@gmail.com>
2017-12-08 22:26:09 +0100

    empty level

--- Projects/NodeTodo/config/index.js
+++
@@ -1,7 +0,0 @@
-var configValues = require('./config');
-
-module.exports = {
-  getDbConnectionString: function() {
-    return 'mongodb://' + configValues.uname + ':' + configValues.pwd + '@ds045511.mlab.com:45511/nodetodosample';
-  }
-}
--- Projects/cli/execs/executable.js
+++
@@ -1,9 +0,0 @@
-function called(a, b) {
-  if (a > b) {
-    console.log('everything seems fine!')
-  } else {
-    console.log('Naaaaa');
-  }
-}
-
-called(10, 2);
